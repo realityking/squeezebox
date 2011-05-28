995985e30b28ea28d08ab13f969ed6b3cc632266
Rouven Weßling <me@rouvenwessling.de>
2011-05-28 22:09:44 +0200

    Use the much better box shadow method in more than just webkit browsers.

--- SqueezeBox.js
+++ SqueezeBox.js
@@ -75,8 +75,12 @@ var SqueezeBox = {
 			styles: {display: 'none', zIndex: this.options.zIndex + 2}
 		});
 		if (this.options.shadow) {
-			if ((Browser.safari && Browser.version >= 3)|| Browser.chrome) {
-				this.win.setStyle('-webkit-box-shadow', '0 0 10px rgba(0, 0, 0, 0.7)');
+			if (Browser.chrome
+			|| (Browser.safari && Browser.version >= 3)
+			|| (Browser.opera && Browser.version >= 10.5)
+			|| (Browser.firefox && Browser.version >= 3.5)
+			|| (Browser.ie && Browser.version >= 9)) {
+				this.win.addClass('shadow');
 			} else if (!Browser.ie6) {
 				var shadow = new Element('div', {'class': 'sbox-bg-wrap'}).inject(this.win);
 				var relay = function(e) {
@@ -124,9 +128,9 @@ var SqueezeBox = {
 
 		if (this.element != null) this.trash();
 		this.element = document.id(subject) || false;
-		
+
 		this.setOptions(Object.merge(this.presets, options || {}));
-		
+
 		if (this.element && this.options.parse) {
 			var obj = this.element.getProperty(this.options.parse);
 			if (obj && (obj = JSON.decode(obj, this.options.parseSecure))) this.setOptions(obj);
@@ -134,7 +138,7 @@ var SqueezeBox = {
 		this.url = ((this.element) ? (this.element.get('href')) : subject) || this.options.url || '';
 
 		this.assignOptions();
-		
+
 		var handler = handler || this.options.handler;
 		if (handler) return this.setContent(handler, this.parsers[handler].call(this, true));
 		var ret = false;
@@ -153,8 +157,8 @@ var SqueezeBox = {
 	},
 
 	assignOptions: function() {
-		this.overlay.set('class', this.options.classOverlay);
-		this.win.set('class', this.options.classWindow);
+		this.overlay.addClass(this.options.classOverlay);
+		this.win.addClass(this.options.classWindow);
 		if (Browser.ie6) this.win.addClass('sbox-window-ie6');
 	},
 
@@ -323,7 +327,6 @@ var SqueezeBox = {
 	handlers: new Hash(),
 
 	parsers: new Hash()
-
 };
 
 SqueezeBox.extend(new Events(function(){})).extend(new Options(function(){})).extend(new Chain(function(){}));
@@ -438,7 +441,6 @@ SqueezeBox.handlers.extend({
 	string: function(str) {
 		return str;
 	}
-
 });
 
 SqueezeBox.handlers.url = SqueezeBox.handlers.ajax;
